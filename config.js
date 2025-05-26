d57e06489ab297df1174a811e94e66fb298853f1
Muhammad Rizky <aksobonde3@gmail.com>
2025-05-26 16:40:11 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,110 @@
+/ Impor modul dan dependensi yang diperlukan
+const {
+    monospace,
+    italic,
+    quote
+} = require("@itsreimau/ckptw-mod");
+
+// Konfigurasi
+global.config = {
+    // Informasi bot dasar
+    bot: {
+        name: "Dragon", // Nama bot
+        prefix:("^\\.", "i"),, // Karakter awalan perintah yang diizinkan
+        phoneNumber: "62858239792311", // Nomor telepon bot (tidak perlu diisi jika menggunakan QR code)
+        thumbnail: "https://files.catbox.moe/3yeevr.jpg", // Gambar thumbnail bot
+        groupJid: "", // JID untuk group bot (opsional jika tidak menggunakan requireBotGroupMembership)
+        newsletterJid: "120363202832071259@newsletter", // JID untuk saluran bot
+
+        // Konfigurasi autentikasi sesi bot
+        authAdapter: {
+            adapter: "default", // Adapter untuk menyimpan sesi (Pilihan adapter: default, mysql, mongo, firebase)
+
+            // Konfigurasi default
+            default: {
+                authDir: "state"
+            },
+
+            // Konfigurasi MySQL
+            mysql: {
+                host: "localhost:3306", // Nama host
+                user: "root", // Nama pengguna
+                password: "admin123", // Kata sandi
+                database: "ckptw-wabot" // Nama database
+            },
+
+            // Konfigurasi MongoDB
+            mongodb: {
+                url: "mongodb://localhost:27017/ckptw-wabot" // URL
+            },
+
+            // Konfigurasi Firebase
+            firebase: {
+                tableName: "ckptw-wabot", // Nama tabel
+                session: "state" // Nama sesi
+            }
+        }
+    },
+
+    // Pesan bot yang disesuaikan untuk situasi tertentu
+    msg: {
+        admin: quote("⛔ Kocak banget member satu ini 😹🫵!"), // Pesan saat perintah hanya untuk admin
+        banned: quote("⛔ lu di ban deck 😹!"), // Pesan untuk pengguna yang dibanned
+        botAdmin: quote("⛔ Tidak dapat memproses karena bot bukan admin grup ini!"), // Pesan jika bot bukan admin di grup
+        botGroupMembership: quote(`⛔ Tidak dapat memproses karena Anda tidak bergabung dengan grup bot! Ketik ${monospace("/botgroup")} untuk mendapatkan tautan grup bot.`),
+        coin: quote("⛔ Tidak dapat memproses karena koin Anda tidak cukup!"), // Pesan saat koin tidak cukup
+        cooldown: quote("🔄 Perintah ini sedang dalam cooldown, tunggu..."), // Pesan saat cooldown perintah
+        gamerestrict: quote("⛔ Tidak dapat memproses karena grup ini membatasi game!"),
+        group: quote("⛔ Perintah hanya dapat diakses dalam grup!"), // Pesan untuk perintah grup
+        owner: quote("⛔ lu siapa mek 😹!"), // Pesan untuk perintah yang hanya owner bisa akses
+        premium: quote("⛔ Tidak dapat memproses karena Anda bukan pengguna Premium!"), // Pesan jika pengguna bukan Premium
+        private: quote("⛔ Perintah hanya dapat diakses dalam obrolan pribadi!"), // Pesan untuk perintah obrolan pribadi
+        restrict: quote("⛔ Perintah ini telah dibatasi karena alasan keamanan!"), // Pesan pembatasan perintah
+
+        readmore: "\u200E".repeat(4001), // String read more
+        note: "“Lorem ipsum dolor sit amet, tenebris in umbra, vitae ad mortem.”", // Catatan
+        footer: italic("Developed by ItsReimau"), // Footer di pesan bot
+
+        wait: quote("🔄 Tunggu sebentar..."), // Pesan loading
+        notFound: quote("❎ Tidak ada yang ditemukan! Coba lagi nanti."), // Pesan item tidak ditemukan
+        urlInvalid: quote("❎ URL tidak valid!") // Pesan jika URL tidak valid
+    },
+
+    // Informasi owner bot
+    owner: {
+        name: "", // Nama owner bot
+        organization: "", // Nama organisasi owner bot
+        id: "", // Nomor telepon owner bot
+        co: [""] // Nomor co-owner bot
+    },
+
+    // Stiker bot
+    sticker: {
+        packname: "", // Nama paket stiker
+        author: "333333 <sukianjirt>" // Pembuat stiker
+    },
+
+    // Sistem bot
+    system: {
+        alwaysOnline: true, // Bot selalu berstatus "online"
+        antiCall: true, // Bot secara otomatis membanned orang yang menelepon
+        autoMention: true, // Bot otomatis mention seseorang dalam pesan yang dikirim
+        autoAiLabel: true, // Bot otomatis memamaki label AI dalam pesan yang dikirim (hanya berfungsi di chat private)
+        autoRead: true, // Bot baca pesan otomatis
+        autoTypingOnCmd: true, // Tampilkan status "sedang mengetik" saat memproses perintah
+        cooldown: 10 * 1000, // Jeda antar perintah (ms)
+        maxListeners: 50, // Max listeners untuk events
+        port: 3000, // Port (jika pakai server)
+        reportErrorToOwner: true, // Laporkan kesalahan ke owner bot
+        restrict: false, // Batasi akses perintah
+        requireBotGroupMembership: false, // Harus gabung grup bot
+        selfOwner: false, // Bot jadi owner sendiri
+        selfReply: true, // Bot balas pesan bot sendiri
+        timeZone: "Asia/Jakarta", // Zona waktu bot
+        uploaderHost: "Nyxs", // Host uploader untuk menyimpan media (Tersedia: Catbox, Cloudku, Erhabot, FastUrl, IDNet, Litterbox, Nyxs, Pomf, Quax, Ryzen, Shojib, TmpErhabot, Uguu, Videy)
+        useCoin: true, // Pakai koin
+        usePairingCode: false, // Pakai kode pairing untuk koneksi
+        customPairingCode: "UMBR4L15", // Kode pairing kustom untuk koneksi (tidak perlu diisi jika menggunakan QR code, jika kosong kode pairing akan random)
+        useServer: false // Jalankan bot dengan server
+    }
+};
